ab3f59d709821697916b8e98123bd91779d2335e
Kim HyunWoo <stories2@naver.com>
2022-04-20 18:36:37 +0900

    ᆫㅣᆼㅏㅣㅏ

--- src/app/views/runtime/runtime-flow2.component.ts
+++ src/app/views/runtime/runtime-flow2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpService } from '../../services/http.services';
@@ -8,7 +8,9 @@ import { RuntimeCardComponent } from './components/runtime-card.component';
     templateUrl: 'runtime-flow2.component.html',
     styleUrls: ['./runtime-flow2.component.css']
 })
-export class RuntimeFlow2Component implements OnInit {
+export class RuntimeFlow2Component implements OnInit, AfterViewInit {
+
+    @ViewChild('dynamicLineSvg') dynamicLineSvg: ElementRef;
 
     playGroundWidth = 3000
     playGroundHeight = 3000
@@ -27,8 +29,8 @@ export class RuntimeFlow2Component implements OnInit {
         {
             type: 'card',
             ref: 'asdf',
-            x: 100,
-            y: 150,
+            x: 205,
+            y: 419,
             w: 0,
             h: 0,
             orderNum: 1
@@ -36,8 +38,8 @@ export class RuntimeFlow2Component implements OnInit {
         {
             type: 'link',
             ref: 'ffasdf',
-            x: 500,
-            y: 250,
+            x: 950,
+            y: 380,
             w: 0,
             h: 0,
             orderNum: 99999
@@ -58,6 +60,31 @@ export class RuntimeFlow2Component implements OnInit {
         private cdr: ChangeDetectorRef,
         private httpService: HttpService) {
     }
+
+    ngAfterViewInit(): void {
+        this.drawLines()
+    }
+
+    draw(): void {
+        this.drawLines()
+    }
+
+    drawLines() {
+        const lines = []
+        const calcLines = this.calcDrawLine()
+        // console.log('calcLines', calcLines, this.childComponents)
+        calcLines.forEach((line, idx) => {
+            // ;left:${line.x1}px;top:${line.y1}px
+            // ;left:${this.childComponents[idx + 1].w / 2}px;top:${this.childComponents[idx + 1].h / 2}px
+            lines.push(`<svg style="position:absolute;width:${this.playGroundWidth}px;height:${this.playGroundHeight}px"><line 
+            x1="${line.x1}" 
+            y1="${line.y1}" 
+            x2="${line.x2}" 
+            y2="${line.y2}" stroke="red" stroke-width="3"/></svg>`)
+        })
+        this.dynamicLineSvg.nativeElement.innerHTML = lines.join('')
+        // console.log('svg', this.dynamicLineSvg.nativeElement.innerHTML)
+    }
     
     ngOnInit(): void {
     }
@@ -77,7 +104,37 @@ export class RuntimeFlow2Component implements OnInit {
         if (this.focusedChildComponent) {
             this.checkBlockDuplicated(this.focusedChildComponent)
             this.checkIfOutOfPlayground(this.focusedChildComponent)
+            this.updateFocusedChildComponentPos(this.focusedChildComponent)
         }
+        this.draw()
+    }
+
+    updateFocusedChildComponentPos(focusedChildComponent) {
+        const tmp = this.childComponents.filter(i => i.ref === focusedChildComponent.id)
+        if (tmp.length <= 0) {
+            return
+        }
+        const childIdx = this.childComponents.indexOf(tmp[0])
+        this.childComponents[childIdx].x = focusedChildComponent.x
+        this.childComponents[childIdx].y = focusedChildComponent.y
+        this.childComponents[childIdx].w = focusedChildComponent.w
+        this.childComponents[childIdx].h = focusedChildComponent.h
+    }
+
+    calcDrawLine() {
+        if (this.childComponents.length <= 1) {
+            return []
+        }
+        return new Array(this.childComponents.length - 1).fill(this.childComponents.keys()).map((val, idx) => {
+            // console.log(document.getElementById(this.childComponents[idx].ref).getBoundingClientRect(),
+            // document.getElementById(this.childComponents[idx + 1].ref).getBoundingClientRect())
+            return {
+                x1: this.childComponents[idx].x + (this.childComponents[idx].w) / 2,
+                y1: this.childComponents[idx].y + (this.childComponents[idx].h) / 2,
+                x2: this.childComponents[idx + 1].x + (this.childComponents[idx + 1].w) / 2,
+                y2: this.childComponents[idx + 1].y + (this.childComponents[idx + 1].h) / 2
+            }
+        })
     }
 
     checkIfOutOfPlayground(focusedChildComponent) {
