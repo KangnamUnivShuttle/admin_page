f9868e1059002ab64bb2377d7b4994ed0bb3bdc2
Kim HyunWoo <stories2@naver.com>
2022-04-20 15:32:54 +0900

    asdf

--- src/app/views/runtime/components/runtime-search.component.ts
+++ src/app/views/runtime/components/runtime-search.component.ts
@@ -1,10 +1,43 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { HttpService } from '../../../services/http.services';
+import { BlockImageModel } from '../block.model';
 
 @Component({
     selector: 'runtime-search',
     templateUrl: 'runtime-search.component.html'
 })
 export class RuntimeSearchComponent implements OnInit {
+
+    pluginImageList: BlockImageModel[] = [
+        {
+            githubUrl: 'https://github.com/KangnamUnivShuttle/plugin_simple_toss.git',
+            imageId: 1,
+            name: 'toss',
+            orderNum: 1,
+        } as BlockImageModel
+    ]
+
+    searchImageName = '';
+    searchForm = this.formBuilder.group({
+        searchImageName: new FormControl(this.searchImageName, [Validators.required]),
+    })
+
+    constructor(private formBuilder: FormBuilder,
+                private httpService: HttpService) {}
+
     ngOnInit(): void {
+    }
+
+    onSearchSubmit() {
+        this.httpService.reqGet('plugin', {'name': this.searchImageName}).toPromise()
+        .then(res => {
+            // console.log(res)
+            this.pluginImageList = res.data
+        })
+    }
+
+    onBtnOpenUrlInNewTab(url: string) {
+        window.open(url, '_blank').focus();
     }
 }
