ab2f9af9917dff6279b639908c883dfa40ecc747
Kim HyunWoo <stories2@naver.com>
2022-04-25 20:37:59 +0900

    Update runtime-search.component.ts

--- src/app/views/runtime/components/runtime-search.component.ts
+++ src/app/views/runtime/components/runtime-search.component.ts
@@ -1,45 +1,54 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { HttpService } from '../../../services/http.services';
-import { BlockImageModel } from '../block.model';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormBuilder, FormControl, Validators } from "@angular/forms";
+import { HttpService } from "../../../services/http.services";
+import { BlockImageModel } from "../block.model";
 
 @Component({
-    selector: 'runtime-search',
-    templateUrl: 'runtime-search.component.html'
+  selector: "runtime-search",
+  templateUrl: "runtime-search.component.html",
 })
 export class RuntimeSearchComponent implements OnInit {
+  pluginImageList: BlockImageModel[] = [
+    // {
+    //     githubUrl: 'https://github.com/KangnamUnivShuttle/plugin_simple_toss.git',
+    //     imageId: 1,
+    //     name: 'toss',
+    //     orderNum: 1,
+    // } as BlockImageModel
+  ];
 
-    pluginImageList: BlockImageModel[] = [
-        {
-            githubUrl: 'https://github.com/KangnamUnivShuttle/plugin_simple_toss.git',
-            imageId: 1,
-            name: 'toss',
-            orderNum: 1,
-        } as BlockImageModel
-    ]
+  searchImageName = "";
+  searchForm = this.formBuilder.group({
+    searchImageName: new FormControl(this.searchImageName, [
+      Validators.required,
+    ]),
+  });
 
-    searchImageName = '';
-    searchForm = this.formBuilder.group({
-        searchImageName: new FormControl(this.searchImageName, [Validators.required]),
-    })
+  @Output() onDragStartPos: EventEmitter<{
+    x: number;
+    y: number;
+    id: string;
+    ele: any;
+  }> = new EventEmitter();
 
-    @Output() onDragStartPos: EventEmitter<{x: number, y: number, id: string, ele: any}> = new EventEmitter();
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpService: HttpService
+  ) {}
 
-    constructor(private formBuilder: FormBuilder,
-                private httpService: HttpService) {}
+  ngOnInit(): void {}
 
-    ngOnInit(): void {
-    }
+  onSearchSubmit() {
+    this.httpService
+      .reqGet("plugin", { name: this.searchImageName })
+      .toPromise()
+      .then((res) => {
+        // console.log(res)
+        this.pluginImageList = res.data;
+      });
+  }
 
-    onSearchSubmit() {
-        this.httpService.reqGet('plugin', {'name': this.searchImageName}).toPromise()
-        .then(res => {
-            // console.log(res)
-            this.pluginImageList = res.data
-        })
-    }
-
-    onBtnOpenUrlInNewTab(url: string) {
-        window.open(url, '_blank').focus();
-    }
+  onBtnOpenUrlInNewTab(url: string) {
+    window.open(url, "_blank").focus();
+  }
 }
