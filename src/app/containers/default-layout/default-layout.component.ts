8bab14d1be8eb0fc6eeb29720ca14329456136c5
Kim HyunWoo <stories2@naver.com>
2022-04-27 00:05:51 +0900

    asdfas

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,15 +1,28 @@
-import {Component} from '@angular/core';
-import { navItems } from '../../_nav';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { HttpService } from "../../services/http.services";
+import { navItems } from "../../_nav";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './default-layout.component.html'
+  selector: "app-dashboard",
+  templateUrl: "./default-layout.component.html",
 })
 export class DefaultLayoutComponent {
   public sidebarMinimized = false;
   public navItems = navItems;
 
+  constructor(private httpService: HttpService, private router: Router) {}
+
   toggleMinimize(e) {
     this.sidebarMinimized = e;
+  }
+
+  onBtnLogoutClicked() {
+    this.httpService
+      .reqDelete("auth", null)
+      .toPromise()
+      .then((res) => {
+        this.router.navigate(["/login"]);
+      });
   }
 }
